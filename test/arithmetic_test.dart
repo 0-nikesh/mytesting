f3e79aca33803903f0b51200d72d9afe1b1be4e6
nikesh <bhandarinikesh93@gmail.com>
2025-02-04 11:46:30 +0545

    groupin done

--- test/arithmetic_test.dart
+++ test/arithmetic_test.dart
@@ -2,15 +2,41 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mytesting/model/arithmetic.dart';
 
 void main() {
-  test('add two numbers', () {
+  group("Arithmetic test", () {
+    test('subtract two numbers', () {
+    final arithemetic = Arithmetic();
+    int expectedValue = -1;
+    arithemetic.first = 3;
+    arithemetic.second = 4;
+    int actualValue = arithemetic.subtract()!;
+    expect(expectedValue, actualValue);
+  });
+
+    test('add two numbers', () {
     final arithemetic = Arithmetic();
     int expectedValue = 7;
     arithemetic.first = 3;
     arithemetic.second = 4;
-    // int actualValue = arithemetic.add()!;
-
-    int actualValue = arithemetic.multiply()!;
-
+    int actualValue = arithemetic.add()!;
     expect(expectedValue, actualValue);
   });
+  });
+
+  // test('multiply two numbers', () {
+  //   final arithemetic = Arithmetic();
+  //   int expectedValue = 12;
+  //   arithemetic.first = 3;
+  //   arithemetic.second = 4;
+  //   int actualValue = arithemetic.multiply()!;
+  //   expect(expectedValue, actualValue);
+  // });
+
+  //   test('division two numbers', () {
+  //   final arithemetic = Arithmetic();
+  //   int expectedValue = 1;
+  //   arithemetic.first = 4;
+  //   arithemetic.second = 4;
+  //   int actualValue = arithemetic.divide()!;
+  //   expect(expectedValue, actualValue);
+  // });
 }
